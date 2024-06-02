804a09e7eaf2e2b0ef73f80616ef42e9527e4d89
a1qs <ankiller2007.09.20@gmail.com>
2024-06-02 22:24:03 +0200

    make cake vaults notify the player everytime a cake has been clicked

---
+++ src/main/java/com/a1qs/the_vault_extras/mixins/MixinCakeHuntObjective.java
@@ -0,0 +1,43 @@
+package com.a1qs.the_vault_extras.mixins;
+
+
+import iskallia.vault.world.vault.VaultRaid;
+import iskallia.vault.world.vault.logic.objective.CakeHuntObjective;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.server.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(value = CakeHuntObjective.class, remap = false)
+public class MixinCakeHuntObjective {
+
+
+
+    @Inject(
+            method = "expandVault",
+            at = @At(value="HEAD"),
+            cancellable = true
+    )
+    private void addTextToNonModifyingCakes(ServerWorld world, ServerPlayerEntity player, BlockPos cakePos, VaultRaid vault, CallbackInfo ci) {
+        ITextComponent c0 = player.getDisplayName().deepCopy().mergeStyle(TextFormatting.LIGHT_PURPLE);
+        ITextComponent c1 = (new StringTextComponent(" found a ")).mergeStyle(TextFormatting.GRAY);
+        ITextComponent c2 = (new StringTextComponent("cake")).mergeStyle(TextFormatting.GREEN);
+        ITextComponent ct = (new StringTextComponent("")).appendSibling(c0).appendSibling(c1).appendSibling(c2);
+        vault.getPlayers().forEach((vPlayer) -> vPlayer.runIfPresent(world.getServer(), (sPlayer) -> sPlayer.sendMessage(ct, Util.DUMMY_UUID)));
+    }
+
+
+
+
+    /*
+
+     */
+}
