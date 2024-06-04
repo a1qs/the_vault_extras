19cb2f6a7d68f9d687b6650ac4bc4af3e9269781
a1qs <ankiller2007.09.20@gmail.com>
2024-06-04 19:21:18 +0200

    Modify MixinCakeHuntObjective to play a sound instead of printing a chat message

--- src/main/java/com/a1qs/the_vault_extras/mixins/MixinCakeHuntObjective.java
+++ src/main/java/com/a1qs/the_vault_extras/mixins/MixinCakeHuntObjective.java
@@ -1,9 +1,12 @@
 package com.a1qs.the_vault_extras.mixins;
 
 
+import com.a1qs.the_vault_extras.events.ModSoundEvents;
 import iskallia.vault.world.vault.VaultRaid;
 import iskallia.vault.world.vault.logic.objective.CakeHuntObjective;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
@@ -20,11 +23,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinCakeHuntObjective {
 
     @Inject(method = "expandVault", at = @At(value="HEAD"), cancellable = true)
-    private void addTextToNonModifyingCakes(ServerWorld world, ServerPlayerEntity player, BlockPos cakePos, VaultRaid vault, CallbackInfo ci) {
+    private void playSoundOnCakeClick(ServerWorld world, ServerPlayerEntity player, BlockPos cakePos, VaultRaid vault, CallbackInfo ci) {
+        /*
         ITextComponent c0 = player.getDisplayName().deepCopy().mergeStyle(TextFormatting.LIGHT_PURPLE);
         ITextComponent c1 = (new StringTextComponent(" found a ")).mergeStyle(TextFormatting.GRAY);
         ITextComponent c2 = (new StringTextComponent("cake")).mergeStyle(TextFormatting.GREEN);
         ITextComponent ct = (new StringTextComponent("")).appendSibling(c0).appendSibling(c1).appendSibling(c2);
         vault.getPlayers().forEach((vPlayer) -> vPlayer.runIfPresent(world.getServer(), (sPlayer) -> sPlayer.sendMessage(ct, Util.DUMMY_UUID)));
+         */
+
+        vault.getPlayers().forEach((vPlayer) -> vPlayer.runIfPresent(world.getServer(), (sPlayer) -> sPlayer.getEntityWorld().playSound(null, player.getPosX(), player.getPosY(), player.getPosZ(), SoundEvents.BLOCK_NOTE_BLOCK_BELL, SoundCategory.PLAYERS, 0.6F, 1.0F)));
     }
 }
