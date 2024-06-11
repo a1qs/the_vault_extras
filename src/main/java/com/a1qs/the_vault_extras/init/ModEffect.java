c8088dc03133e53646690c2c3ece593bc587cda3
a1qs <ankiller2007.09.20@gmail.com>
2024-06-11 21:53:52 +0200

    add Chest Rarity Effect

---
+++ src/main/java/com/a1qs/the_vault_extras/init/ModEffect.java
@@ -0,0 +1,27 @@
+package com.a1qs.the_vault_extras.init;
+
+import com.a1qs.the_vault_extras.VaultExtras;
+import iskallia.vault.effect.BasicEffect;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+
+import java.awt.Color;
+
+
+public class ModEffect {
+    public static final Effect EXTRA_CHEST_RARITY;
+
+
+    public ModEffect() {
+    }
+
+    public static void register(RegistryEvent.Register<Effect> event) {
+        event.getRegistry().registerAll(new Effect[]{EXTRA_CHEST_RARITY});
+    }
+
+    static {
+        EXTRA_CHEST_RARITY = new BasicEffect(EffectType.BENEFICIAL, Color.green.getRGB(), new ResourceLocation(VaultExtras.MOD_ID, "extra_chest_rarity"));
+    }
+}
