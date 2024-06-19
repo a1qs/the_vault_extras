540792df4255f5ba8fa8d858c14b4faf7037f625
a1qs <ankiller2007.09.20@gmail.com>
2024-06-19 20:54:27 +0200

    remove redundant casts

--- src/main/java/com/a1qs/the_vault_extras/init/ModConfigs.java
+++ src/main/java/com/a1qs/the_vault_extras/init/ModConfigs.java
@@ -5,12 +5,14 @@ import com.a1qs.the_vault_extras.config.MysteryRuneConfig;
 
 public class ModConfigs {
     public static void register() {
-        MYSTERY_RUNE = (MysteryRuneConfig) (new MysteryRuneConfig()).readConfig();
-        MYSTERY_BOOK = (MysteryBookConfig) (new MysteryBookConfig()).readConfig();
+        MYSTERY_RUNE = new MysteryRuneConfig().readConfig();
+        MYSTERY_BOOK = new MysteryBookConfig().readConfig();
     }
 
     public static MysteryRuneConfig MYSTERY_RUNE;
     public static MysteryBookConfig MYSTERY_BOOK;
 
-    public static void registerCompressionConfigs() {};
+    public static void registerCompressionConfigs() {
+
+    }
 }
