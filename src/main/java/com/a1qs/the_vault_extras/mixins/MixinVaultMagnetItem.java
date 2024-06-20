353d55e69dff119fd57c4cd2a3f5c2b2f48ff44b
a1qs <ankiller2007.09.20@gmail.com>
2024-06-20 20:41:15 +0200

    refactor code for less repetitiveness, make vault magnets work with unbreaking/unbreakable

--- src/main/java/com/a1qs/the_vault_extras/mixins/MixinVaultMagnetItem.java
+++ src/main/java/com/a1qs/the_vault_extras/mixins/MixinVaultMagnetItem.java
@@ -1,43 +1,145 @@
 package com.a1qs.the_vault_extras.mixins;
 
+import iskallia.vault.config.DurabilityConfig;
+import iskallia.vault.init.ModConfigs;
 import iskallia.vault.init.ModItems;
 import iskallia.vault.item.VaultMagnetItem;
+import iskallia.vault.skill.talent.TalentTree;
+import iskallia.vault.skill.talent.type.UnbreakableTalent;
+import iskallia.vault.world.data.PlayerTalentsData;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.theillusivec4.curios.api.CuriosApi;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.UUID;
+
+import static iskallia.vault.item.VaultMagnetItem.isMagnet;
+
 @Mixin(value = VaultMagnetItem.class, remap = false)
 public abstract class MixinVaultMagnetItem {
-    @Inject(method = "onItemPickup", at = @At(value="HEAD"), cancellable = true)
 
-    private static void magnetApplyDurabilityDamageInCurio(PlayerEvent.ItemPickupEvent event, CallbackInfo ci) {
+    private static final Random random = new Random();
+
+    @Final
+    @Shadow
+    private static final HashMap<UUID, UUID> pulledItems = new HashMap();
+
+    /**
+     * @author a1qs
+     * @reason Curios Compatablility & making Unbreaking work with Magnets
+     */
+    @SubscribeEvent
+    @Overwrite
+    public static void onItemPickup(PlayerEvent.ItemPickupEvent event) {
         PlayerEntity player = event.getPlayer();
         PlayerInventory inventory = player.inventory;
+        pulledItems.remove(event.getOriginalEntity().getUniqueID());
+        int unbreakingLevel;
+        DurabilityConfig cfg = ModConfigs.DURBILITY;
 
-        // check if there's a magnet in a curio slot
-        if(CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, ModItems.VAULT_MAGNET_OMEGA).isPresent()) {
-            ItemStack stack = CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, ModItems.VAULT_MAGNET_OMEGA).get().getStack();
 
-            //damage the item if it's enabled upon pickup
-            if(stack.getOrCreateTag().getBoolean("Enabled")) {
-                stack.damageItem(1, player, (onBroken) -> {});
-            }
-        } else if(CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, ModItems.VAULT_MAGNET_STRONG).isPresent()) {
-            ItemStack stack = CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, ModItems.VAULT_MAGNET_STRONG).get().getStack();
-            if(stack.getOrCreateTag().getBoolean("Enabled")) {
-                stack.damageItem(1, player, (onBroken) -> {});
-            }
-        } else if(CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, ModItems.VAULT_MAGNET_WEAK).isPresent()) {
-            ItemStack stack = CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, ModItems.VAULT_MAGNET_WEAK).get().getStack();
-            if(stack.getOrCreateTag().getBoolean("Enabled")) {
-                stack.damageItem(1, player, (onBroken) -> {});
+
+
+        for(int i = 0; i < inventory.getSizeInventory(); ++i) {
+            ItemStack stack = inventory.getStackInSlot(i);
+            if (!stack.isEmpty()) {
+                if (isMagnet(stack) && stack.getOrCreateTag().getBoolean("Enabled")) {
+
+                    unbreakingLevel = calculateUnbreakingLevel(player, stack);
+                    float chance = cfg.getDurabilityIgnoreChance(unbreakingLevel);
+                    if(random.nextFloat() < chance) {
+                        stack.damageItem(1, player, (onBroken) -> {
+                        });
+                    }
+                } else {
+                    LazyOptional<IItemHandler> itemHandler = stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY);
+                    itemHandler.ifPresent((h) -> {
+                        for(int j = 0; j < h.getSlots(); ++j) {
+                            ItemStack stackInHandler = h.getStackInSlot(j);
+                            if (isMagnet(stackInHandler) && stack.getOrCreateTag().getBoolean("Enabled")) {
+                                int handlerUnbreakingLevel = calculateUnbreakingLevel(player, stack);
+                                float chance = cfg.getDurabilityIgnoreChance(handlerUnbreakingLevel);
+                                if(random.nextFloat() < chance) {
+                                    stackInHandler.damageItem(1, player, (onBroken) -> {
+                                    });
+                                }
+                            }
+                        }
+
+                    });
+                }
             }
         }
+
+        Item[] vaultMagnets = {ModItems.VAULT_MAGNET_OMEGA, ModItems.VAULT_MAGNET_STRONG, ModItems.VAULT_MAGNET_WEAK};
+        for (Item magnet : vaultMagnets) {
+            if (CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, magnet).isPresent()) {
+                ItemStack stack = CuriosApi.getCuriosHelper().findFirstCurio(inventory.player, magnet).get().getStack();
+
+                if (stack.getOrCreateTag().getBoolean("Enabled")) {
+
+                    unbreakingLevel = calculateUnbreakingLevel(player, stack);
+                    float chance = cfg.getDurabilityIgnoreChance(unbreakingLevel);
+                    if(random.nextFloat() < chance) {
+                        stack.damageItem(1, player, (onBroken) -> {
+                        });
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    /**
+     * @author a1qs
+     * @reason make Vault Magnets enchantable
+     */
+    @Overwrite
+    public boolean isEnchantable(ItemStack stack) {
+       return true;
+    }
+
+    /**
+     * @author a1qs
+     * @reason make Vault Magnets enchantable with Unbreaking
+     */
+    @Overwrite
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        return EnchantmentHelper.getEnchantments(book).containsKey(Enchantments.UNBREAKING);
+    }
+
+    private static int calculateUnbreakingLevel(PlayerEntity player, ItemStack stack) {
+        int unbreakingLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.UNBREAKING, stack);
+
+        if(player instanceof ServerPlayerEntity) {
+            unbreakingLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.UNBREAKING, stack);
+            ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+            TalentTree abilities = PlayerTalentsData.get(serverPlayer.getServerWorld()).getTalents(serverPlayer);
+            UnbreakableTalent talent;
+            for (Iterator var6 = abilities.getTalents(UnbreakableTalent.class).iterator(); var6.hasNext(); unbreakingLevel = (int) ((float) unbreakingLevel + talent.getExtraUnbreaking())) {
+                talent = (UnbreakableTalent) var6.next();
+            }
+        }
+        return unbreakingLevel;
     }
 }
