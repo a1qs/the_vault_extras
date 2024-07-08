88142c9429238469013b5d15986a10cd0903d769
a1qs <ankiller2007.09.20@gmail.com>
2024-07-08 15:28:13 +0200

    draw arrow in the JEI Recipecategory

--- src/main/java/com/a1qs/the_vault_extras/integration/RecyclerRecipeCategory.java
+++ src/main/java/com/a1qs/the_vault_extras/integration/RecyclerRecipeCategory.java
@@ -3,10 +3,14 @@ package com.a1qs.the_vault_extras.integration;
 import com.a1qs.the_vault_extras.VaultExtras;
 import com.a1qs.the_vault_extras.data.recipes.RecyclerRecipe;
 import com.a1qs.the_vault_extras.init.ModBlocks;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.category.IRecipeCategory;
@@ -16,19 +20,28 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 
-
 public class RecyclerRecipeCategory implements IRecipeCategory<RecyclerRecipe> {
-
 
     public static final ResourceLocation UID = new ResourceLocation(VaultExtras.MOD_ID, "recycle");
     public static final ResourceLocation TEXTURE = new ResourceLocation(VaultExtras.MOD_ID, "textures/gui/vault_recycler_jei.png");
     private final IDrawable background;
     private final IDrawable icon;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
 
     public RecyclerRecipeCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 0, 0, 175, 41);
         this.icon = helper.createDrawableIngredient(new ItemStack(ModBlocks.VAULT_RECYCLER.get()));
+        this.cachedArrows = CacheBuilder.newBuilder()
+            .maximumSize(25)
+            .build(new CacheLoader<Integer, IDrawableAnimated>() {
+                @Override
+                public IDrawableAnimated load(Integer smeltTime) {
+                    return helper.drawableBuilder(TEXTURE, 176, 0, 24, 17)
+                            .buildAnimated(smeltTime, IDrawableAnimated.StartDirection.LEFT, false);
+                }
+            });
     }
+
 
     @Override
     public ResourceLocation getUid() {
@@ -59,18 +72,14 @@ public class RecyclerRecipeCategory implements IRecipeCategory<RecyclerRecipe> {
     public void setIngredients(RecyclerRecipe recipe, IIngredients ingredients) {
         ingredients.setInputIngredients(recipe.getIngredients());
         ingredients.setOutput(VanillaTypes.ITEM, recipe.getRecipeOutput());
-
     }
 
-
-
-    //todo: draw the arrow while it progresses
-    // check the other JEI implementations
     @Override
     public void draw(RecyclerRecipe recipe, MatrixStack matrixStack, double mouseX, double mouseY) {
         Minecraft.getInstance().fontRenderer.drawString(matrixStack, TextFormatting.DARK_GRAY + "" + (int)(recipe.getExtraChance()*100) + "%",98, 29, 0xFFFFFF);
+        IDrawableAnimated arrow = getArrow(recipe);
+        arrow.draw(matrixStack, 52, 12);
     }
-
 
     @Override
     public void setRecipe(IRecipeLayout recipeLayout, RecyclerRecipe recipe, IIngredients ingredients) {
@@ -82,12 +91,8 @@ public class RecyclerRecipeCategory implements IRecipeCategory<RecyclerRecipe> {
         recipeLayout.getItemStacks().set(ingredients);
     }
 
-    /*
-    @Override
-    public List<ITextComponent> getTooltipStrings(RecyclerRecipe recipe, double mouseX, double mouseY) {
-        if (mouseX >= 70 && mouseX <= 105 && mouseY >= 25 && mouseY <= 32)
-            return Collections.singletonList(new StringTextComponent("Cost Range"));
-        return new ArrayList<>();
+    private IDrawableAnimated getArrow(RecyclerRecipe recipe) {
+        int smeltTime = recipe.getSmeltTime();
+        return this.cachedArrows.getUnchecked(smeltTime);
     }
-    */
 }
