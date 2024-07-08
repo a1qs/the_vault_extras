6d1e6ce3ab5d017a1cdff5e3a7bd52867cdbd8bc
a1qs <ankiller2007.09.20@gmail.com>
2024-07-08 12:45:04 +0200

    remove SanctifiedPedestalTile

--- src/main/java/com/a1qs/the_vault_extras/block/SanctifiedPedestalBlock.java
+++ src/main/java/com/a1qs/the_vault_extras/block/SanctifiedPedestalBlock.java
@@ -1,8 +1,6 @@
 package com.a1qs.the_vault_extras.block;
 
-import com.a1qs.the_vault_extras.block.tileentity.SanctifiedPedestalTile;
 import com.a1qs.the_vault_extras.init.ModParticles;
-import com.a1qs.the_vault_extras.init.ModTileEntities;
 import iskallia.vault.attribute.EnumAttribute;
 import iskallia.vault.attribute.IntegerAttribute;
 import iskallia.vault.init.ModAttributes;
@@ -15,15 +13,12 @@ import net.minecraft.item.Items;
 import net.minecraft.particles.RedstoneParticleData;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 import java.util.Random;
@@ -38,41 +33,33 @@ public class SanctifiedPedestalBlock extends Block {
         this.setDefaultState(this.stateContainer.getBaseState().with(USED, Boolean.valueOf(false)));
     }
 
-    //todo: write the state to the SanctifiedPedestalBlock#setUsed method
     @Override
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         if(!worldIn.isRemote) {
+            ItemStack stack = player.getHeldItemMainhand();
+            if(stack.getItem() instanceof IdolItem) {
+                if(!state.get(USED)) {
 
-            TileEntity tileEntity = worldIn.getTileEntity(pos);
+                    Optional<EnumAttribute<VaultGear.State>> attribute = ModAttributes.GEAR_STATE.get(stack);
+                    if (attribute.isPresent() && ((EnumAttribute)attribute.get()).getValue(stack) == VaultGear.State.IDENTIFIED) {
+                        IntegerAttribute modifiersToRoll = ModAttributes.GEAR_MAX_LEVEL.get(stack).get();
+                        if(modifiersToRoll.getValue(stack) <= 10) {
+                            ModAttributes.GEAR_MAX_LEVEL.create(stack, modifiersToRoll.getValue(stack) + 2);
+                            player.playSound(SoundEvents.BLOCK_BEACON_POWER_SELECT, SoundCategory.PLAYERS, 1.0F, 0.75F);
+                            player.spawnSweepParticles();
 
-            if(tileEntity instanceof SanctifiedPedestalTile) {
-                SanctifiedPedestalTile pedestal = (SanctifiedPedestalTile) tileEntity;
-                ItemStack stack = player.getHeldItemMainhand();
-                if(stack.getItem() instanceof IdolItem) {
-                    if(!state.get(USED)) {
-
-                        Optional<EnumAttribute<VaultGear.State>> attribute = ModAttributes.GEAR_STATE.get(stack);
-                        if (attribute.isPresent() && ((EnumAttribute)attribute.get()).getValue(stack) == VaultGear.State.IDENTIFIED) {
-                            IntegerAttribute modifiersToRoll = ModAttributes.GEAR_MAX_LEVEL.get(stack).get();
-                            if(modifiersToRoll.getValue(stack) <= 10) {
-                                ModAttributes.GEAR_MAX_LEVEL.create(stack, modifiersToRoll.getValue(stack) + 2);
-                                player.playSound(SoundEvents.BLOCK_BEACON_POWER_SELECT, SoundCategory.PLAYERS, 1.0F, 0.75F);
-                                player.spawnSweepParticles();
-
-                                BlockState newState = state.with(USED, true);
-                                worldIn.setBlockState(pos, newState, 3);
-                                pedestal.setUsed(true);
-                            } else {
-                                player.playSound(SoundEvents.BLOCK_ANVIL_LAND, SoundCategory.PLAYERS, 1.0F, 1.5F);
-                                player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "The Pedestal lacks the power to enhance this Idol further."), true);
-                            }
+                            BlockState newState = state.with(USED, true);
+                            worldIn.setBlockState(pos, newState, 3);
+                        } else {
+                            player.playSound(SoundEvents.BLOCK_ANVIL_LAND, SoundCategory.PLAYERS, 1.0F, 1.5F);
+                            player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "The Pedestal lacks the power to enhance this Idol further."), true);
                         }
                     }
-                } else if (!(stack.getItem() == Items.AIR) && !state.get(USED)) {
-                    player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "This item doesnt seem to work.. perhaps I should try something god-related"), true);
-                } else {
-                    player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "Nothing happened, maybe I should try using an item"), true);
                 }
+            } else if (!(stack.getItem() == Items.AIR) && !state.get(USED)) {
+                player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "This item doesnt seem to work.. perhaps I should try something god-related"), true);
+            } else {
+                player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "Nothing happened, maybe I should try using an item"), true);
             }
         }
         return ActionResultType.SUCCESS;
@@ -98,17 +85,5 @@ public class SanctifiedPedestalBlock extends Block {
             worldIn.addParticle(RedstoneParticleData.REDSTONE_DUST, pos.getX() + 0.15D, pos.getY() + 1.1D, pos.getZ() + 0.85D, 0.0D, 0.01D, 0.0D);
             worldIn.addParticle(RedstoneParticleData.REDSTONE_DUST, pos.getX() + 0.15D, pos.getY() + 1.1D, pos.getZ() + 0.15D, 0.0D, 0.01D, 0.0D);
         }
-    }
-
-
-    @Nullable
-    @Override
-    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-        return ModTileEntities.SANCTIFIED_PEDESTAL_TILE.get().create();
-    }
-
-    @Override
-    public boolean hasTileEntity(BlockState state) {
-        return true;
     }
 }
