409a1cb2d30b682f2d8b4ffcd6a1a6b8d08f43a9
a1qs <ankiller2007.09.20@gmail.com>
2024-08-10 21:13:02 +0200

    Delete the dimension upon exiting the vault and if no one is inside the vault & add it as a config option

--- src/main/java/com/a1qs/the_vault_extras/config/VaultExtrasConfig.java
+++ src/main/java/com/a1qs/the_vault_extras/config/VaultExtrasConfig.java
@@ -6,12 +6,16 @@ public final class VaultExtrasConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
     public static final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_COOP_FAVOURS;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_VAULT_DELETION;
 
     static {
         BUILDER.push("Configuration file for The Vault Extras");
 
-        ENABLE_COOP_FAVOURS = BUILDER.comment("EXPERIMENTAL: Enable Favour changes for Vaults in Coop Vaults")
+        ENABLE_COOP_FAVOURS = BUILDER.comment("Enable Favour changes for Vaults in Coop Vaults")
                 .define("ENABLE_COOP_FAVOURS", true);
+
+        ENABLE_VAULT_DELETION = BUILDER.comment("EXPERIMENTAL: Enable Vaults being deleted upon leaving them & noone being inside the Vault dimension")
+                .define("ENABLE_VAULT_DELETION", false);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
---
+++ src/main/java/com/a1qs/the_vault_extras/events/DeleteDimensionEvent.java
@@ -0,0 +1,79 @@
+package com.a1qs.the_vault_extras.events;
+
+import com.a1qs.the_vault_extras.VaultExtras;
+import com.a1qs.the_vault_extras.config.VaultExtrasConfig;
+import iskallia.vault.Vault;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.FolderName;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Mod.EventBusSubscriber
+public class DeleteDimensionEvent {
+
+    @SubscribeEvent
+    public static void onDimensionChange(PlayerEvent.PlayerChangedDimensionEvent event) {
+        if(VaultExtrasConfig.ENABLE_VAULT_DELETION.get()) {
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            RegistryKey<World> dimensionKey = getDimensionToRemove();
+            if(event.getFrom() == dimensionKey) {
+                ServerWorld dimension = server.getWorld(dimensionKey);
+                if (dimension != null && dimension.getPlayers().isEmpty()) {
+                    deleteDimensionData(server, dimensionKey);
+                }
+            }
+        }
+    }
+
+    private static void deleteDimensionData(MinecraftServer server, RegistryKey<World> dimensionKey){
+        Path dimensionsPath = server.func_240776_a_(new FolderName("dimensions"));
+        Path vaultPath = dimensionsPath.resolve("the_vault").resolve("vault").normalize();
+        Path poiPath = vaultPath.resolve("poi").normalize();
+        Path regionPath = vaultPath.resolve("region").normalize();
+        Path vaultRaidFilePath = server.func_240776_a_(new FolderName("data")).resolve("the_vault_VaultRaid.dat").normalize();
+
+        try {
+            deleteDirectoryRecursively(poiPath.toFile(), null);
+            deleteDirectoryRecursively(regionPath.toFile(), null);
+            if(Files.exists(vaultRaidFilePath)) {
+                Files.deleteIfExists(vaultRaidFilePath);
+                VaultExtras.LOGGER.info("Deleted VaultRaid file: {}", vaultRaidFilePath);
+            } else {
+                VaultExtras.LOGGER.warn("VaultRaid file not found: {}", vaultRaidFilePath);
+            }
+            VaultExtras.LOGGER.info("Deleted Dimension: {}", dimensionKey.getLocation());
+        } catch (IOException e) {
+            VaultExtras.LOGGER.error("Failed to delete dimension directory: {}", e.getMessage());
+        }
+    }
+
+    private static void deleteDirectoryRecursively(File directory, Path keepPath) throws IOException {
+        if(directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for(File file : files) {
+                    if(!file.toPath().normalize().equals(keepPath)) {
+                        deleteDirectoryRecursively(file, keepPath);
+                    }
+                }
+            }
+        }
+        if(!directory.toPath().normalize().equals(keepPath)) {
+            Files.delete(directory.toPath());
+        }
+    }
+
+    private static RegistryKey<World> getDimensionToRemove() {
+        return Vault.VAULT_KEY;
+    }
+}
