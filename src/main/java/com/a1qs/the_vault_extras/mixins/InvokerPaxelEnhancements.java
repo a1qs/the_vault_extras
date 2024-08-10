2965cf797db94ee5d018d63a956c2bbf142ad3c7
a1qs <ankiller2007.09.20@gmail.com>
2024-08-11 01:59:31 +0200

    access registry method of PaxelEnhancements.class

---
+++ src/main/java/com/a1qs/the_vault_extras/mixins/InvokerPaxelEnhancements.java
@@ -0,0 +1,14 @@
+package com.a1qs.the_vault_extras.mixins;
+
+import iskallia.vault.item.paxel.enhancement.PaxelEnhancement;
+import iskallia.vault.item.paxel.enhancement.PaxelEnhancements;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(value = PaxelEnhancements.class, remap = false)
+public interface InvokerPaxelEnhancements {
+    @Invoker("register")
+    static <T extends PaxelEnhancement> T invokeRegister(String name, T enhancement) {
+        throw new AssertionError();
+    }
+}
