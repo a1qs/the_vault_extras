b39280a6c1ec23d1732f25c7e743de2def77e32e
a1qs <ankiller2007.09.20@gmail.com>
2024-09-03 08:26:30 +0200

    Add durability tooltip

---
+++ src/main/java/com/a1qs/the_vault_extras/mixins/MixinVaultArmorItem.java
@@ -0,0 +1,28 @@
+package com.a1qs.the_vault_extras.mixins;
+
+import iskallia.vault.item.gear.VaultArmorItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.*;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(value = VaultArmorItem.class, remap = false)
+public class MixinVaultArmorItem {
+
+    @Inject(method = "addInformation", at = @At(value = "TAIL"))
+    public void addDuraInfo(ItemStack itemStack, World world, List<ITextComponent> tooltip, ITooltipFlag flag, CallbackInfo ci) {
+        StringTextComponent durabilityLabel = new StringTextComponent("Durability: ");
+        durabilityLabel.setStyle(Style.EMPTY.applyFormatting(TextFormatting.WHITE));
+
+        StringTextComponent durabilityValues = new StringTextComponent(itemStack.getMaxDamage() - itemStack.getDamage() + "/" + itemStack.getMaxDamage());
+        durabilityValues.setStyle(Style.EMPTY.applyFormatting(TextFormatting.GRAY));
+
+            tooltip.add(durabilityLabel.appendSibling(durabilityValues));
+    }
+}
---
+++ src/main/java/com/a1qs/the_vault_extras/mixins/MixinVaultSwordItem.java
@@ -0,0 +1,31 @@
+package com.a1qs.the_vault_extras.mixins;
+
+import iskallia.vault.item.gear.VaultSwordItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(value = VaultSwordItem.class, remap = false)
+public class MixinVaultSwordItem {
+
+    @Inject(method = "addInformation", at = @At(value = "TAIL"))
+    public void addDuraInfo(ItemStack itemStack, World world, List<ITextComponent> tooltip, ITooltipFlag flag, CallbackInfo ci) {
+        StringTextComponent durabilityLabel = new StringTextComponent("Durability: ");
+        durabilityLabel.setStyle(Style.EMPTY.applyFormatting(TextFormatting.WHITE));
+
+        StringTextComponent durabilityValues = new StringTextComponent(itemStack.getMaxDamage() - itemStack.getDamage() + "/" + itemStack.getMaxDamage());
+        durabilityValues.setStyle(Style.EMPTY.applyFormatting(TextFormatting.GRAY));
+
+        tooltip.add(durabilityLabel.appendSibling(durabilityValues));
+    }
+}
