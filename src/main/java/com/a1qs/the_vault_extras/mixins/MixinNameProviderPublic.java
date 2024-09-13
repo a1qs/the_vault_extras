c1dc24a61a2e86b1a2ff3d9da388576d9d881953
a1qs <ankiller2007.09.20@gmail.com>
2024-09-13 16:04:31 +0200

    updated remmxx name & Faewild_ name

--- src/main/java/com/a1qs/the_vault_extras/mixins/MixinNameProviderPublic.java
+++ src/main/java/com/a1qs/the_vault_extras/mixins/MixinNameProviderPublic.java
@@ -5,6 +5,7 @@ import iskallia.vault.util.NameProviderPublic;
 import net.minecraftforge.common.UsernameCache;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +13,12 @@ import java.util.List;
 @Mixin(value = NameProviderPublic.class, remap = false)
 public class MixinNameProviderPublic {
 
-    private static final List<String> DEV_NAMES = Lists.newArrayList(new String[]{"KaptainWutax", "iGoodie", "jmilthedude", "Scalda", "Kumara22", "Goktwo", "Aolsen96", "Winter_Grave", "kimandjax", "Monni_21", "Starmute", "MukiTanuki", "RowanArtifex", "HellFirePvP", "Pau1_", "Douwsky", "pomodoko", "Damnsecci"});
-    private static final List<String> SMP_S2 = Lists.newArrayList(new String[]{"CaptainSparklez", "Stressmonster101", "CaptainPuffy", "AntonioAsh", "ItsFundy", "iskall85", "Tubbo_", "HBomb94", "5uppps", "X33N", "PeteZahHutt", "Seapeekay"});
-    private static final List<String> DEV_NAMES_VH2POINT5 = Lists.newArrayList(new String[]{"a1qs", "JoshWannaPaas", "monkyman640", "xD0R0x", "Faewild_", "remmxx", "RMZing"});
+    @Unique
+    private static final List<String> DEV_NAMES = Lists.newArrayList("KaptainWutax", "iGoodie", "jmilthedude", "Scalda", "Kumara22", "Goktwo", "Aolsen96", "Winter_Grave", "kimandjax", "Monni_21", "Starmute", "MukiTanuki", "RowanArtifex", "HellFirePvP", "Pau1_", "Douwsky", "pomodoko", "Damnsecci");
+    @Unique
+    private static final List<String> SMP_S2 = Lists.newArrayList("CaptainSparklez", "Stressmonster101", "CaptainPuffy", "AntonioAsh", "ItsFundy", "iskall85", "Tubbo_", "HBomb94", "5uppps", "X33N", "PeteZahHutt", "Seapeekay");
+    @Unique
+    private static final List<String> DEV_NAMES_VH2POINT5 = Lists.newArrayList("a1qs", "JoshWannaPaas", "monkyman640", "xD0R0x", "faewild_", "MELANCHOLIC88", "RMZing");
 
     /**
      * @author a1qs
@@ -22,7 +26,7 @@ public class MixinNameProviderPublic {
      */
     @Overwrite
     public static List<String> getAllAvailableNames() {
-        List<String> names = new ArrayList();
+        List<String> names = new ArrayList<>();
         names.addAll(DEV_NAMES_VH2POINT5);
         names.addAll(DEV_NAMES);
         names.addAll(SMP_S2);
@@ -30,8 +34,9 @@ public class MixinNameProviderPublic {
         return names;
     }
 
+    @Unique
     private static List<String> getKnownUsernames() {
-        return new ArrayList(UsernameCache.getMap().values());
+        return new ArrayList<>(UsernameCache.getMap().values());
     }
 
 }
