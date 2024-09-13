f0193cd0ea2ab4125b9640106982b17f6dc06b8f
a1qs <ankiller2007.09.20@gmail.com>
2024-09-13 15:34:21 +0200

    made a holdable veinminer key

--- src/main/java/com/a1qs/the_vault_extras/events/InputEvents.java
+++ src/main/java/com/a1qs/the_vault_extras/events/InputEvents.java
@@ -4,6 +4,9 @@ import com.a1qs.the_vault_extras.VaultExtras;
 import com.a1qs.the_vault_extras.init.ModKeyBinds;
 import com.a1qs.the_vault_extras.network.MagnetMessage;
 import com.a1qs.the_vault_extras.network.VaultExtrasNetwork;
+import iskallia.vault.init.ModNetwork;
+import iskallia.vault.network.message.AbilityKeyMessage;
+import iskallia.vault.network.message.AbilityQuickselectMessage;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
@@ -30,6 +33,18 @@ public class InputEvents {
     private static void onInput(Minecraft mc, int key, int action) {
         if(mc.currentScreen == null && ModKeyBinds.magnetToggle.isPressed()) {
             VaultExtrasNetwork.CHANNEL.sendToServer(new MagnetMessage(key));
+        }
+
+        if (mc.currentScreen == null && key != -1) {
+            if (ModKeyBinds.veinminerHold.getKey().getKeyCode() == key) {
+                if (action == 0) {
+
+                    ModNetwork.CHANNEL.sendToServer(new AbilityKeyMessage(true, false, false, false));
+                } else if (action == 1) {
+                    ModNetwork.CHANNEL.sendToServer(new AbilityQuickselectMessage("Vein Miner"));
+                    ModNetwork.CHANNEL.sendToServer(new AbilityKeyMessage(false, true, false, false));
+                }
+            }
         }
     }
 }
--- src/main/java/com/a1qs/the_vault_extras/init/ModKeyBinds.java
+++ src/main/java/com/a1qs/the_vault_extras/init/ModKeyBinds.java
@@ -4,16 +4,20 @@ import com.a1qs.the_vault_extras.VaultExtras;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.lwjgl.glfw.GLFW;
 
 import java.awt.event.KeyEvent;
 
 public class ModKeyBinds {
     public static KeyBinding magnetToggle;
+    public static KeyBinding veinminerHold;
 
     public static void register(final FMLClientSetupEvent event) {
-        magnetToggle = create("magnet_toggle", KeyEvent.VK_SUBTRACT);
+        magnetToggle = create("magnet_toggle", GLFW.GLFW_KEY_KP_SUBTRACT);
+        veinminerHold = create("veinminer_hold", GLFW.GLFW_KEY_UNKNOWN);
 
         ClientRegistry.registerKeyBinding(magnetToggle);
+        ClientRegistry.registerKeyBinding(veinminerHold);
     }
 
     private static KeyBinding create (String name, int key) {
