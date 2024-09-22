c485d89da308b74ec7ea408ef7e130cf54d65b71
JoshWannaPaas <paasjoshua@yahoo.com>
2024-09-22 04:12:26 -0700

    idk

--- src/main/java/com/a1qs/the_vault_extras/mixins/MixinVaultRaid.java
+++ src/main/java/com/a1qs/the_vault_extras/mixins/MixinVaultRaid.java
@@ -121,105 +121,106 @@ public class MixinVaultRaid {
         });
     });
 
-    @Final
-    @Shadow
-    public static VaultTask CHECK_BAIL_COOP = VaultTask.register(Vault.id("check_bail_coop_new"), (vault, player, world) -> {
-        if (vault.getTimer().getRunTime() >= 200) {
-            player.runIfPresent(world.getServer(), (sPlayer) -> {
-                if (!vault.getGenerator().getPiecesAt(sPlayer.getPosition(), VaultStart.class).isEmpty()) {
-                    AxisAlignedBB box = sPlayer.getBoundingBox();
-                    BlockPos min = new BlockPos(box.minX + 0.001, box.minY + 0.001, box.minZ + 0.001);
-                    BlockPos max = new BlockPos(box.maxX - 0.001, box.maxY - 0.001, box.maxZ - 0.001);
-                    BlockPos.Mutable pos = new BlockPos.Mutable();
-                    if (world.isAreaLoaded(min, max)) {
-                        for(int i = min.getX(); i <= max.getX(); ++i) {
-                            for(int j = min.getY(); j <= max.getY(); ++j) {
-                                for(int k = min.getZ(); k <= max.getZ(); ++k) {
-                                    BlockState state = world.getBlockState(pos.setPos(i, j, k));
-                                    if (state.getBlock() == ModBlocks.VAULT_PORTAL) {
-                                        if (sPlayer.hasPortalCooldown()) {
-                                            sPlayer.setPortalCooldown();
-                                            return;
-                                        }
-
-                                        if (!vault.canExit(player)) {
-                                            StringTextComponent text = new StringTextComponent("You cannot exit this Vault!");
-                                            text.setStyle(Style.EMPTY.setColor(Color.fromInt(16711680)));
-                                            sPlayer.sendStatusMessage(text, true);
-                                            return;
-                                        }
-
-                                        vault.getAllObjectives().forEach((objective) -> {
-                                            objective.notifyBail(vault, player, world);
-                                        });
-                                        sPlayer.setPortalCooldown();
-
-                                        // Leave party if one exists
-                                        VaultPartyData data = VaultPartyData.get((ServerWorld) sPlayer.world);
-                                        Optional<VaultPartyData.Party> party = data.getParty(sPlayer.getUniqueID());
-                                        if (party.isPresent() && party.get().getMembers().size() > 1) {
-                                            Commands command = new Commands(Commands.EnvironmentType.ALL);
-                                            command.handleCommand(sPlayer.getCommandSource(), "party leave");
-
-                                            REMOVE_SCAVENGER_ITEMS.then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(EXIT_SAFELY).execute(vault, player, world);
-
-                                            // Try to remove from vault objective
-                                            vault.getPlayers().remove(player);
-                                        }
-
-                                        HIDE_OVERLAY.execute(vault, player, world);
-                                        IFormattableTextComponent playerName = sPlayer.getDisplayName().deepCopy();
-                                        playerName.setStyle(Style.EMPTY.setColor(Color.fromInt(9974168)));
-                                        StringTextComponent suffix = new StringTextComponent(" bailed.");
-                                        world.getServer().getPlayerList().func_232641_a_((new StringTextComponent("")).appendSibling(playerName).appendSibling(suffix), ChatType.CHAT, player.getPlayerId());
-                                    }
-                                }
-                            }
-                        }
-
-                    }
-                }
-            });
-        }
-    });
+//    @Final
+//    @Shadow
+//    public static VaultTask CHECK_BAIL_COOP = VaultTask.register(Vault.id("check_bail_coop_new"), (vault, player, world) -> {
+//        if (vault.getTimer().getRunTime() >= 200) {
+//            player.runIfPresent(world.getServer(), (sPlayer) -> {
+//                if (!vault.getGenerator().getPiecesAt(sPlayer.getPosition(), VaultStart.class).isEmpty()) {
+//                    AxisAlignedBB box = sPlayer.getBoundingBox();
+//                    BlockPos min = new BlockPos(box.minX + 0.001, box.minY + 0.001, box.minZ + 0.001);
+//                    BlockPos max = new BlockPos(box.maxX - 0.001, box.maxY - 0.001, box.maxZ - 0.001);
+//                    BlockPos.Mutable pos = new BlockPos.Mutable();
+//                    if (world.isAreaLoaded(min, max)) {
+//                        for(int i = min.getX(); i <= max.getX(); ++i) {
+//                            for(int j = min.getY(); j <= max.getY(); ++j) {
+//                                for(int k = min.getZ(); k <= max.getZ(); ++k) {
+//                                    BlockState state = world.getBlockState(pos.setPos(i, j, k));
+//                                    if (state.getBlock() == ModBlocks.VAULT_PORTAL) {
+//                                        if (sPlayer.hasPortalCooldown()) {
+//                                            sPlayer.setPortalCooldown();
+//                                            return;
+//                                        }
+//
+//                                        if (!vault.canExit(player)) {
+//                                            StringTextComponent text = new StringTextComponent("You cannot exit this Vault!");
+//                                            text.setStyle(Style.EMPTY.setColor(Color.fromInt(16711680)));
+//                                            sPlayer.sendStatusMessage(text, true);
+//                                            return;
+//                                        }
+//
+//                                        vault.getAllObjectives().forEach((objective) -> {
+//                                            objective.notifyBail(vault, player, world);
+//                                        });
+//                                        sPlayer.setPortalCooldown();
+//
+//                                        // Leave party if one exists
+//                                        VaultPartyData data = VaultPartyData.get((ServerWorld) sPlayer.world);
+//                                        Optional<VaultPartyData.Party> party = data.getParty(sPlayer.getUniqueID());
+//                                        if (party.isPresent() && party.get().getMembers().size() > 1) {
+//                                            Commands command = new Commands(Commands.EnvironmentType.ALL);
+//                                            command.handleCommand(sPlayer.getCommandSource(), "party leave");
+//
+//                                            REMOVE_SCAVENGER_ITEMS.then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(EXIT_SAFELY).execute(vault, player, world);
+//
+//                                            // Try to remove from vault objective
+//                                            vault.getPlayers().remove(player);
+//                                        }
+//
+//                                        HIDE_OVERLAY.execute(vault, player, world);
+//                                        IFormattableTextComponent playerName = sPlayer.getDisplayName().deepCopy();
+//                                        playerName.setStyle(Style.EMPTY.setColor(Color.fromInt(9974168)));
+//                                        StringTextComponent suffix = new StringTextComponent(" bailed.");
+//                                        world.getServer().getPlayerList().func_232641_a_((new StringTextComponent("")).appendSibling(playerName).appendSibling(suffix), ChatType.CHAT, player.getPlayerId());
+//                                    }
+//                                }
+//                            }
+//                        }
+//
+//                    }
+//                }
+//            });
+//        }
+//    });
 
     /**
      * @author Josh
      * @reason Remove co-op deaths wiping party
      */
-    @Overwrite
-    public static VaultRaid coop(VaultGenerator generator, VaultTask initializer, RaidProperties properties, VaultObjective objective, List<VaultEvent<?>> events, Map<VaultPlayerType, Set<ServerPlayerEntity>> playersMap) {
-        MinecraftServer srv = (MinecraftServer) LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
-        VaultRaid vault = new VaultRaid(generator, initializer, properties, events, (Iterable)playersMap.entrySet().stream().flatMap((entry) -> {
-            VaultPlayerType type = (VaultPlayerType)entry.getKey();
-            Set<ServerPlayerEntity> players = (Set)entry.getValue();
-            if (type == VaultPlayerType.RUNNER) {
-                return players.stream().map((player) -> {
-                    VaultRunner runner = new VaultRunner(player.getUniqueID());
-                    runner.getBehaviours().add(new VaultBehaviour(NO_OBJECTIVES_LEFT_GLOBALLY.and(NO_TIME_LEFT), REMOVE_SCAVENGER_ITEMS.then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(GRANT_EXP_COMPLETE.then(EXIT_SAFELY))));
-                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER.and(NO_TIME_LEFT), EXIT_DEATH_ALL));
-                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER.and(IS_DEAD), EXIT_DEATH.then(SAVE_SOULBOUND_GEAR).then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(REMOVE_SCAVENGER_ITEMS).then(GRANT_EXP_DEATH)));
-                    runner.getBehaviours().add(new VaultBehaviour(IS_FINISHED.negate(), TICK_SPAWNER.then(TICK_CHEST_PITY)));
-                    runner.getBehaviours().add(new VaultBehaviour(AFTER_GRACE_PERIOD.and(IS_FINISHED.negate()), TICK_INFLUENCES));
-                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER, CHECK_BAIL_COOP));
-                    runner.getBehaviours().add(new VaultBehaviour(NO_ACTIVE_RUNNERS_LEFT, REMOVE_SCAVENGER_ITEMS.then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(GRANT_EXP_BAIL.then(EXIT_SAFELY))));
-//                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER).and(IS_OUTSIDE), )
-                    runner.getProperties().create(SPAWNER, new VaultSpawner());
-                    runner.getProperties().create(CHEST_PITY, new VaultChestPity());
-                    runner.getTimer().start(objective.getVaultTimerStart(ModConfigs.VAULT_GENERAL.getTickCounter()));
-                    return runner;
-                });
-            } else {
-                if (type == VaultPlayerType.SPECTATOR) {
-                }
+//    @Overwrite
+//    public static VaultRaid coop(VaultGenerator generator, VaultTask initializer, RaidProperties properties, VaultObjective objective, List<VaultEvent<?>> events, Map<VaultPlayerType, Set<ServerPlayerEntity>> playersMap) {
+//        MinecraftServer srv = (MinecraftServer) LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
+//        VaultRaid vault = new VaultRaid(generator, initializer, properties, events, (Iterable)playersMap.entrySet().stream().flatMap((entry) -> {
+//            VaultPlayerType type = (VaultPlayerType)entry.getKey();
+//            Set<ServerPlayerEntity> players = (Set)entry.getValue();
+//            if (type == VaultPlayerType.RUNNER) {
+//                return players.stream().map((player) -> {
+//                    VaultRunner runner = new VaultRunner(player.getUniqueID());
+//                    runner.getBehaviours().add(new VaultBehaviour(NO_OBJECTIVES_LEFT_GLOBALLY.and(NO_TIME_LEFT), REMOVE_SCAVENGER_ITEMS.then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(GRANT_EXP_COMPLETE.then(EXIT_SAFELY))));
+//                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER.and(NO_TIME_LEFT), EXIT_DEATH_ALL));
+//                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER.and(IS_DEAD), EXIT_DEATH.then(SAVE_SOULBOUND_GEAR).then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(REMOVE_SCAVENGER_ITEMS).then(GRANT_EXP_DEATH)));
+//                    runner.getBehaviours().add(new VaultBehaviour(IS_FINISHED.negate(), TICK_SPAWNER.then(TICK_CHEST_PITY)));
+//                    runner.getBehaviours().add(new VaultBehaviour(AFTER_GRACE_PERIOD.and(IS_FINISHED.negate()), TICK_INFLUENCES));
+//                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER, CHECK_BAIL_COOP));
+//                    runner.getBehaviours().add(new VaultBehaviour(NO_ACTIVE_RUNNERS_LEFT, REMOVE_SCAVENGER_ITEMS.then(REMOVE_INVENTORY_RESTORE_SNAPSHOTS).then(GRANT_EXP_BAIL.then(EXIT_SAFELY))));
+////                    runner.getBehaviours().add(new VaultBehaviour(IS_RUNNER).and(IS_OUTSIDE), )
+//                    runner.getProperties().create(SPAWNER, new VaultSpawner());
+//                    runner.getProperties().create(CHEST_PITY, new VaultChestPity());
+//                    runner.getTimer().start(objective.getVaultTimerStart(ModConfigs.VAULT_GENERAL.getTickCounter()));
+//                    return runner;
+//                });
+//            } else {
+//                if (type == VaultPlayerType.SPECTATOR) {
+//                }
+//
+//                return Stream.empty();
+//            }
+//        }).collect(Collectors.toList()));
+//        vault.getAllObjectives().add(objective.thenComplete(LEVEL_UP_GEAR).thenComplete(VICTORY_SCENE));
+//        vault.getAllObjectives().forEach((obj) -> {
+//            obj.initialize(srv, vault);
+//        });
+//        return vault;
+//    }
 
-                return Stream.empty();
-            }
-        }).collect(Collectors.toList()));
-        vault.getAllObjectives().add(objective.thenComplete(LEVEL_UP_GEAR).thenComplete(VICTORY_SCENE));
-        vault.getAllObjectives().forEach((obj) -> {
-            obj.initialize(srv, vault);
-        });
-        return vault;
-    }
 }
