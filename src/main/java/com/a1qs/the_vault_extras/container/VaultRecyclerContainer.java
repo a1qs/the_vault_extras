738049bc9babf14cd794d6e0246f099d52ddb68e
a1qs <ankiller2007.09.20@gmail.com>
2024-09-27 18:45:54 +0200

    fixed being able to move items into the output slots of the recycler container

--- src/main/java/com/a1qs/the_vault_extras/container/VaultRecyclerContainer.java
+++ src/main/java/com/a1qs/the_vault_extras/container/VaultRecyclerContainer.java
@@ -126,7 +126,7 @@ public class VaultRecyclerContainer extends Container {
         if (index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
             // This is a vanilla container slot so merge the stack into the tile inventory
             if (!mergeItemStack(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX
-                    + TE_INVENTORY_SLOT_COUNT, false)) {
+                    + 1, false)) {
                 return ItemStack.EMPTY;  // EMPTY_ITEM
             }
         } else if (index < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
